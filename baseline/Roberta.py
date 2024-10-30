2ec81595ed16a44ce0eade8a77b9ea0d1eca0154
Aucki6144 <aucki6144@163.com>
2024-10-30 15:35:42 +0800

    hotfix: roberta support for cuda

--- baseline/Roberta.py
+++ baseline/Roberta.py
@@ -11,34 +11,39 @@ from transformers import RobertaTokenizer, RobertaForSequenceClassification, Ada
 from torch.utils.data import DataLoader, random_split
 import numpy as np
 
+
 def main(args):
     model_name = args.model_name
     data_path = args.data_set
 
-    #Load pretrained model and tokenizer
+    # Load pretrained model and tokenizer
     tokenizer = RobertaTokenizer.from_pretrained(model_name)
-    model = RobertaForSequenceClassification.from_pretrained(model_name, num_labels=5, problem_type="multi_label_classification", ignore_mismatched_sizes=True)
+    model = RobertaForSequenceClassification.from_pretrained(model_name, num_labels=5,
+                                                             problem_type="multi_label_classification",
+                                                             ignore_mismatched_sizes=True)
 
-    #Load the dataset
+    # Load the dataset
     data_df = pd.read_csv(data_path)
 
-    #convert the labels into numpy array
+    # convert the labels into numpy array
     def row_process(row):
         text = row['text']
         labels = np.array([row['Anger'], row['Fear'], row['Joy'], row['Sadness'], row['Surprise']])
         labels = labels.astype(np.float32).tolist()
         return {'text': text, 'labels': labels}
+
     formatted_data = data_df.apply(row_process, axis=1).tolist()
 
     # Construct datasets
     dataset = datasets.Dataset.from_pandas(pd.DataFrame(formatted_data))
     dataset = dataset.train_test_split(test_size=0.1)
     train_set = dataset['train']
-    #train_set = train_set.select(range(0,500))
+    # train_set = train_set.select(range(0,500))
     test_set = dataset['test']
-    #train_set = train_set.select(range(0,100)')
 
-    #preprocess the model_input
+    # train_set = train_set.select(range(0,100)')
+
+    # preprocess the model_input
     def preprocess(example):
         inputs = "Classify the emotion: " + example['text']
         model_inputs = tokenizer(inputs, max_length=128, truncation=True, padding='max_length')
@@ -46,7 +51,7 @@ def main(args):
         attention_mask = torch.tensor(model_inputs['attention_mask'])
         labels = torch.tensor(example['labels'])
         return {'input_ids': input_ids, 'attention_mask': attention_mask, 'labels': labels}
-    
+
     train_set = train_set.map(preprocess).remove_columns("text")
     test_set = test_set.map(preprocess).remove_columns("text")
     train_set.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
@@ -54,32 +59,32 @@ def main(args):
     print(type(train_set['input_ids']))
     print(len(train_set['labels']))
 
-    train_size = int(0.9* len(train_set))
+    train_size = int(0.9 * len(train_set))
     val_size = len(train_set) - train_size
     train_dataset, val_dataset = random_split(train_set, [train_size, val_size])
 
     train_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=True)
     test_dataloader = DataLoader(val_dataset, batch_size=128)
 
-    #train the model
+    # train the model
     optimizer = AdamW(model.parameters(), lr=1e-5)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
     loss_function = torch.nn.CrossEntropyLoss()
 
-    for epoch in range(1):    #choose the number of epoch
+    for epoch in range(1):  # choose the number of epoch
         model.train()
         for i, batch in enumerate(train_dataloader):
-            print(f"{i+1}th batch")
-            inputs = {'input_ids': batch['input_ids'],'attention_mask': batch['attention_mask'],'labels': batch['labels']}
+            print(f"{i + 1}th batch")
+            inputs = {'input_ids': batch['input_ids'].to(device),
+                      'attention_mask': batch['attention_mask'].to(device),
+                      'labels': batch['labels'].to(device)}
             optimizer.zero_grad()
             outputs = model(**inputs)
             logits = outputs.logits
-            loss = loss_function(logits, batch['labels'].float())
+            loss = loss_function(logits, batch['labels'].float().to(device))
             loss.backward()
             optimizer.step()
-
-
 
     if args.skip_save:
         print("Skipping saving model")
@@ -94,7 +99,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--model_name', 
+        '--model_name',
         '-m',
         type=str,
         required=False,
--- baseline/Roberta_evalution.py
+++ baseline/Roberta_evalution.py
@@ -11,24 +11,27 @@ import torch
 from sklearn.metrics import precision_recall_fscore_support, hamming_loss
 from torch.utils.data import DataLoader
 import numpy as np
+
+
 def evalution(args):
-    #load the model
+    # load the model
     tokenizer = RobertaTokenizer.from_pretrained(args.model_path)
     model = RobertaForSequenceClassification.from_pretrained(args.model_path)
     data_path = args.data_set
 
-    #data prepossessing
+    # data prepossessing
     data_df = pd.read_csv(data_path)
+
     def row_process(row):
         text = row['text']
         labels = np.array([row['Anger'], row['Fear'], row['Joy'], row['Sadness'], row['Surprise']])
         labels = labels.astype(np.float32).tolist()
         return {'text': text, 'labels': labels}
-    
 
     formatted_data = data_df.apply(row_process, axis=1).tolist()
     test_set = datasets.Dataset.from_pandas(pd.DataFrame(formatted_data))
-    #test_set = test_set.select(range(0,500))
+
+    # test_set = test_set.select(range(0,500))
 
     def preprocess(example):
         inputs = "Classify the emotion: " + example['text']
@@ -37,28 +40,31 @@ def evalution(args):
         attention_mask = torch.tensor(model_inputs['attention_mask'])
         labels = np.array(example['labels']).astype(np.float32)
         return {'input_ids': input_ids, 'attention_mask': attention_mask, 'labels': labels}
+
     test_set = test_set.map(preprocess).remove_columns("text")
     y_true = test_set['labels']
     test_set.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
     test_dataloader = DataLoader(test_set, batch_size=128)
 
-    #Prediction
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    # Prediction
     model.eval()
     y_pred = []
     threshold = 0.5
     for i, batch in enumerate(test_dataloader):
         with torch.no_grad():
-            print(f"{i+1}th batch")
-            inputs = {'input_ids': batch['input_ids'],
-                    'attention_mask': batch['attention_mask']}
+            print(f"{i + 1}th batch")
+            inputs = {'input_ids': batch['input_ids'].to(device),
+                      'attention_mask': batch['attention_mask'].to(device)}
             outputs = model(**inputs)
             logits = outputs.logits
-            predicted_labels = (torch.sigmoid(logits) > threshold).float().numpy()
+            predicted_labels = (torch.sigmoid(logits) > threshold).float().cpu().numpy()
             print(predicted_labels)
             print(batch['labels'])
-            y_pred.extend(predicted_labels) 
+            y_pred.extend(predicted_labels)
 
-    #metrics evalution
+            # metrics evalution
     precision, recall, f1, _ = precision_recall_fscore_support(y_true, y_pred, average='micro', zero_division=1)
     accuracy = 1 - hamming_loss(y_true, y_pred)
     print(f"Accuracy: {accuracy}")
@@ -66,11 +72,6 @@ def evalution(args):
     print(f"Recall: {recall}")
     print(f"F1 Score: {f1}")
     return
-
-
-
-
-
 
 
 if __name__ == '__main__':
@@ -93,7 +94,6 @@ if __name__ == '__main__':
         default='./public_data/train/track_a/eng.csv',
         help='Path to evaluation dataset',
     )
-
 
     config_args = parser.parse_args()
 
